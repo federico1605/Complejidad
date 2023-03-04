3135985e7edddb85d455ce535844fa6a4ec56116
Federico zapata <fede.zapata16@gmail.com>
2023-03-04 09:50:34 -0500

    termostato terminado aceptado por pagina

--- Termostato/mainTermostato.py
+++ Termostato/mainTermostato.py
@@ -1,9 +1,17 @@
 t = int(input())  # Cantidad de veces que va a calcular
-count = 0
-
+result = []
 for i in range(t):
-    x = int(input())
-    r = int(input())
-    L = int(input())
-    a = int(input())
-    b = int(input())
+    l,r,x = map(int,input().split())
+    a,b = map(int,input().split())
+    if a == b:
+        result.append(0)
+    elif abs(a - b) >= x:
+        result.append(1)
+    elif r - max(a,b) >= x or min(a, b) - l >= x:
+        result.append(2)
+    elif r - min(a, b) >= x and max(a,b) - l >= x:
+        result.append(3)
+    else:
+        result.append(-1)
+for temp in result:
+    print(temp)
