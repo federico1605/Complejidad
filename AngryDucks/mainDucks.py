23bba8118cf0b4d0501119a271cf2e3b0f87e961
Federico Zapata <fede.zapata16@gmail.com>
2023-03-13 15:05:34 -0500

    Se documento el codigo de Angry Ducks

--- AngryDucks/mainDucks.py
+++ AngryDucks/mainDucks.py
@@ -1,20 +1,26 @@
+'''
+Este codigo es la solucion para el reto de beecrown de Angry duck's
+se soluciono con fomrulas basicas de fisica cinematica, de forma
+que se respetaron la logica de la parte fisica, se entregaron datos basicos 
+para los calculo como lo fue el pi y la gravedad.
+'''
 from math import cos
 from math import sin
 pi =  3.14159
 gravity = 9.80665
-heightBegin = float(input())
-p1,p2 = map(int,input().split())
-n = int(input())
+heightBegin = float(input()) 
+p1,p2 = map(int,input().split()) #Acotacion de la ciudad
+n = int(input()) # Numeros de intentos.
 for attempts in range(n):
     angle,vo = map(float,input().split())
-    angle = angle * pi / 180
-    vox= vo * cos(angle)
+    angle = angle * pi / 180 # Se pasa el angulo a radianes para mantener todo en el mismo sistema de unidades
+    vox= vo * cos(angle) 
     voy= vo * sin(angle)
     heightMax = (voy**2)/(gravity*2)
     vfy = pow(2*gravity*heightMax,1/2)
-    tv = voy / gravity
-    tf = vfy / gravity
-    tt = tv + tf
+    tv = voy / gravity #Tiempo de vuelo
+    tf = vfy / gravity #Tiempo de bajada
+    tt = tv + tf #Tiempo total
     postionFinal = vox * tt
     result =  round(postionFinal, 5)
 
